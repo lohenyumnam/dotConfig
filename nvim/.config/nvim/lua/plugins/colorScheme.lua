e2e1e8b740b86175b4308b9424111d04fa6eb9ae
Lohen <lohensana@gmail.com>
2025-01-13 22:57:36 +0530

    chore: update nvim colorsheme and plugins

---
+++ nvim/.config/nvim/lua/plugins/colorScheme.lua
@@ -0,0 +1,57 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = true,
+    -- priority = 1000,
+    config = true,
+    opts = ...,
+  },
+  -- material
+  {
+    "marko-cerovac/material.nvim",
+    name = "material",
+    lazy = true,
+    -- priority = 1000,
+    opts = {
+      disable = {
+        background = true,
+        term_colors = true,
+        -- eob_lines = true,
+      },
+    },
+  },
+  -- Catppuccin
+  {
+    "catppuccin/nvim",
+    priority = 1000,
+    name = "catppuccin",
+    opts = {
+      transparentbackground = true,
+    },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+          end
+        end,
+      },
+    },
+  },
+  -- rose-pine
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "gruvbox",
+      colorscheme = "catppuccin",
+      -- colorscheme = "material",
+    },
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,15 @@
+return {
+    -- file explorer
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = {
+            filesystem = {
+                filtered_items = {
+                    hide_dotfiles = false,
+                    visible = true, -- All "hide" just mean "dimmed out"
+                    hide_gitignored = false,
+                },
+            },
+        },
+    },
+}
