f1082c9d86d3f03ca2e191e2f429bb14d491d911
Lohen <lohensana@gmail.com>
2025-01-16 21:48:55 +0530

    chore: setup tsserver

--- nvim/.config/nvim/lua/plugins/typescript-tools.lua
+++ nvim/.config/nvim/lua/plugins/typescript-tools.lua
@@ -4,7 +4,6 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {
       settings = {
-
         tsserver_file_preferences = {
           includeInlayParameterNameHints = "all",
           includeCompletionsForModuleExports = true,
