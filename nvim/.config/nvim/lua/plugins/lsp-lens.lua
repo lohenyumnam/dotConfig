363146e379c972c78539c3be94b0b13f472d6d47
Lohen <lohensana@gmail.com>
2025-01-16 21:48:33 +0530

    chore: disable lsp-lens

--- nvim/.config/nvim/lua/plugins/lsp-lens.lua
+++ nvim/.config/nvim/lua/plugins/lsp-lens.lua
@@ -1,6 +1,8 @@
 return {
-  "VidocqH/lsp-lens.nvim",
-  config = function()
-    require("lsp-lens").setup({})
-  end,
+  {
+    -- "VidocqH/lsp-lens.nvim",
+    -- config = function()
+    --   require("lsp-lens").setup({})
+    -- end,
+  },
 }
