aaabbe309896bfe6c057071c6d8e2ff5ea2320c8
Lohen Yumnam <lohenYumnam@happiestMinds.com>
2025-01-17 18:04:18 +0530

    chore: remove typescript-tool

--- nvim/.config/nvim/lua/plugins/typescript-tools.lua
+++ nvim/.config/nvim/lua/plugins/typescript-tools.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {
-      settings = {
-        tsserver_file_preferences = {
-          includeInlayParameterNameHints = "all",
-          includeCompletionsForModuleExports = true,
-          ...,
-        },
-      },
-    },
-  },
+  -- {
+  --   "pmizio/typescript-tools.nvim",
+  --   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  --   opts = {
+  --     settings = {
+  --       tsserver_file_preferences = {
+  --         includeInlayParameterNameHints = "all",
+  --         includeCompletionsForModuleExports = true,
+  --         ...,
+  --       },
+  --     },
+  --   },
+  -- },
 }
