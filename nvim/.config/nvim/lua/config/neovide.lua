85d743abfbb86b32d423eabca8e9b3259aae4ed6
Lohen <lohensana@gmail.com>
2025-01-21 00:23:53 +0530

    feat: neovide config added

---
+++ nvim/.config/nvim/lua/config/neovide.lua
@@ -0,0 +1,8 @@
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  -- vim.o.guifont = "Operator Mono Lig,JetBrains Mono:h14" -- text below applies for VimScript
+
+  -- vim.opt.linespace = 13
+  vim.o.linespace = 15
+  -- vim.g.neovide_scale_factor = 1
+end
