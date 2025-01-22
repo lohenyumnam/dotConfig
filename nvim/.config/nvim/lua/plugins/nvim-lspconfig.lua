c8d1b5124887ee9b6fca3eb8107474f30a09b07c
Lohen <lohensana@gmail.com>
2025-01-23 00:01:16 +0530

    feat: add ts setting

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,9 +1,37 @@
 return {
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   priority = 1000,
-  --   config = function()
-  --     require("lspconfig").quick_lint_js.setup({})
-  --   end,
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    priority = 1000,
+    opts = {
+      servers = {
+        vtsls = {
+          settings = {
+            -- List of Settings
+            -- https://github.com/yioneko/vtsls/blob/main/packages/service/configuration.schema.json
+            typescript = {
+              updateImportsOnFileMove = { enabled = "always" },
+              suggest = {
+                completeFunctionCalls = true,
+              },
+              inlayHints = {
+                enumMemberValues = { enabled = true },
+                -- functionLikeReturnTypes = { enabled = true },
+                parameterNames = { enabled = "all" },
+                parameterTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                variableTypes = { enabled = false },
+                referencesCodeLens = {
+                  showOnAllFunctions = { enabled = false },
+                  enabled = false,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    -- config = function()
+    --   require("lspconfig").quick_lint_js.setup({})
+    -- end,
+  },
 }
